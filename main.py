6d5a5143b408fd2d9fbc63c65e46fadde53b1387
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-05-17 13:06:37 +0100

    make variables good names (no comments gang)

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="neovim", url="https://ligmaspoons.tk"))
+    bot_presence = await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="neovim", url="https://ligmaspoons.tk"))
 
 @bot.command(description="edit latest pic")
 async def edit(ctx, input_image_link: str):
-    await ctx.respond("editing...")
-    requests_input_image = requests.get(str(input_image_link))
-    input_image = rfc.requests_response_to_filename(requests_input_image)
+    reply_loading = await ctx.respond("editing...")
+    downloaded_input_image = requests.get(str(input_image_link))
+    input_image = rfc.requests_response_to_filename(downloaded_input_image)
     with open(input_image, 'wb') as input_image_file:
         input_image_file.write(requests.get(input_image_link).content)
     convert_command = "convert -background none " + input_image + " images/jerome_head_small.png -layers flatten output.png"
-    subprocess.run(convert_command, shell=True)
-    await ctx.send(file=discord.File("output.png"))
-    subprocess.run("rm -rf " + input_image, shell=True)
-    subprocess.run("rm -rf output.png", shell=True)
+    convert_shell_process = subprocess.run(convert_command, shell=True)
+    send_image_file = await ctx.send(file=discord.File("output.png"))
+    delete_downloaded_image = subprocess.run("rm -fr " + input_image, shell=True)
+    delete_output_image = subprocess.run("rm -fr output.png", shell=True)
 
 @bot.command(description="run commands")
 async def cmd(ctx, command: str):
